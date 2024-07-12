f798c64f04eec01494f2669088dd189d38c1c754
nekonabesan <sheepdogs_2@hotmail.com>
2024-07-13 00:12:22 +0900

    2024/07/13

--- Modules.py
+++ Modules.py
@@ -57,7 +57,7 @@ class Modules:
             fig_ax[0].set_ylabel('Gain [dB]')
 
             fig_ax[1].grid(which="both", ls=':')
-            fig_ax[1].set_xlabel('$\omega$ [rad/s]')
+            fig_ax[1].set_xlabel('$\\omega$ [rad/s]')
             fig_ax[1].set_ylabel('Phase [deg]')
             
             if len(args) > 0:
